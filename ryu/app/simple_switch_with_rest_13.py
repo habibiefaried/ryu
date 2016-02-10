d37189cde4d5c205761f3ff976dca8190d663465
Habibie Faried <habibiefaried@gmail.com>
2016-02-10 08:56:58 +0700

    Rest sudah bisa yey

--- ryu/app/simple_switch_with_rest_13.py
+++ ryu/app/simple_switch_with_rest_13.py
@@ -7,7 +7,6 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.app.wsgi import ControllerBase, WSGIApplication, route
-from ryu.lib import dpid as dpid_lib
 
 simple_switch_instance_name = 'simple_switch_api_app'
 
@@ -26,7 +25,6 @@ class SimpleSwitchRest13(simple_switch_13.SimpleSwitch13):
 		super(SimpleSwitchRest13, self).switch_features_handler(ev)
 		datapath = ev.msg.datapath
 		self.switches[datapath.id] = datapath #dpid
-		print datapath.id
 		self.mac_to_port.setdefault(datapath.id, {}) #dpid
 
 	def set_mac_to_port(self, dpid, entry):
@@ -52,9 +50,12 @@ class SimpleSwitchRest13(simple_switch_13.SimpleSwitch13):
 					self.add_flow(datapath, 1, match, actions)
 
 				mac_table.update({entry_mac : entry_port})
-		return mac_table
+				return mac_table
+		else:
+			return {"pesan": "Datapath not found"}
 
 #harus diganti keknya
+##Cuma masalah tipe data untuk pengaksesan
 class SimpleSwitchController(ControllerBase):
 	def __init__(self, req, link, data, **config):
 		super(SimpleSwitchController,self).__init__(req, link, data,**config)
@@ -65,32 +66,34 @@ class SimpleSwitchController(ControllerBase):
 		print req
 		print kwargs
 		body = {};
-		body["Hello"] = "World"
+		body["pesan"] = "Welcome to Switch API with OpenFlow 1.3"
 		return Response(content_type='application/json', body=json.dumps(body))
+	
+	@route('simpleswitch', '/simpleswitch/mactable',methods='[GET]')
+	def list_all_mac_table(self, req, **kwargs):
+		return Response(content_type='application/json', body=json.dumps(self.simpl_switch_spp.mac_to_port))
 
-	@route('simpleswitch', '/simpleswitch/mactable/{dpid}', methods=['GET'], requirements={'dpid': dpid_lib.DPID_PATTERN})
+	@route('simpleswitch', '/simpleswitch/mactable/{dpid}', methods=['GET'])
 	def list_mac_table(self, req, **kwargs):
-		simple_switch = self.simpl_switch_spp
-		dpid = dpid_lib.str_to_dpid(kwargs['dpid'])
+		dpid = int(kwargs['dpid'])
+		
+		if dpid not in self.simpl_switch_spp.mac_to_port:
+			return Response(status=404, body="DPID Not Found")
 
-		if dpid not in simple_switch.mac_to_port:
-			return Response(content_type='application/json', body=json.dumps(simple_switch))
-
-		mac_table = simple_switch.mac_to_port.get(dpid, {})
+		mac_table = self.simpl_switch_spp.mac_to_port.get(dpid, {})
 		return Response(content_type='application/json', body=json.dumps(mac_table))
 
-	@route('simpleswitch', '/simpleswitch/mactable/{dpid}',  methods=['PUT'], requirements={'dpid': dpid_lib.DPID_PATTERN})
+	@route('simpleswitch', '/simpleswitch/mactable/{dpid}',  methods=['POST'])
 	def put_mac_table(self, req, **kwargs):
-		simple_switch = self.simpl_switch_spp
-		dpid = dpid_lib.str_to_dpid(kwargs['dpid'])
+		dpid = int(kwargs['dpid'])
 		new_entry = eval(req.body)
 
-		if dpid not in simple_switch.mac_to_port:
-			return Response(status=404)
+		if dpid not in self.simpl_switch_spp.mac_to_port:
+			return Response(status=404, body="DPID Not Found")
 
 		try:
-			mac_table = simple_switch.set_mac_to_port(dpid, new_entry)
+			mac_table = self.simpl_switch_spp.set_mac_to_port(dpid, new_entry)
 			body = json.dumps(mac_table)
 			return Response(content_type='application/json', body=body)
 		except Exception as e:
-			return Response(status=500)
+			return Response(status=500, body="Internal Server Error")
